0e63e8b34580baa67f4e90fe4aa5945d4c414942
Thomas Bandixen <thomas@bandixen.ch>
2015-01-06 19:19:31 +0100

    updated

--- wp-content/themes/videoproject/header.php
+++ wp-content/themes/videoproject/header.php
@@ -5,9 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>VideoProject Theme</title>
-    <link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() ); ?>/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-
 
     <?php wp_head(); ?>
 </head>
