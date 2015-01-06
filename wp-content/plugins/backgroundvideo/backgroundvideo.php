8d20c549027d71a20d32de233fb5ca64c031e98c
Thomas Bandixen <thomas@bandixen.ch>
2015-01-06 19:20:26 +0100

    updated

--- wp-content/plugins/backgroundvideo/backgroundvideo.php
+++ wp-content/plugins/backgroundvideo/backgroundvideo.php
@@ -25,7 +25,7 @@ class BackgroundVideo{
       //wp_register_script( 'jquery.tubular', esc_url( get_template_directory_uri() ).'/js/jquery.tubular.1.0.js' );
 
       wp_enqueue_script( 'bootstrap', esc_url( get_template_directory_uri() ).'/js/bootstrap.min.js', array('jquery') );
-      wp_enqueue_style( 'bootstrap.css', esc_url( get_template_directory_uri() ).'/css/bootstrap.min.css', array('bootstrap') );
+      wp_enqueue_style( 'bootstrap.css', esc_url( get_template_directory_uri() ).'/css/bootstrap.min.css' );
 
       wp_enqueue_script( 'jquery.tubular', esc_url( get_template_directory_uri() ).'/js/jquery.tubular.1.0.js', array('jquery') );
     }
