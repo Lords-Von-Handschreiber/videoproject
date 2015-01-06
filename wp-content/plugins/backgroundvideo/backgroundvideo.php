d3d0681dbc5954e8209a2ca406c70664d39a3382
Thomas Bandixen <thomas@bandixen.ch>
2015-01-06 19:26:23 +0100

    updated

--- wp-content/plugins/backgroundvideo/backgroundvideo.php
+++ wp-content/plugins/backgroundvideo/backgroundvideo.php
@@ -23,11 +23,6 @@ class BackgroundVideo{
     public static function enqueue_and_register_my_scripts() {
       //wp_register_script( 'bootstrap', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js' );
       //wp_register_script( 'jquery.tubular', esc_url( get_template_directory_uri() ).'/js/jquery.tubular.1.0.js' );
-
-      wp_enqueue_script( 'bootstrap', esc_url( get_template_directory_uri() ).'/js/bootstrap.min.js', array('jquery') );
-      wp_enqueue_style( 'bootstrap.css', esc_url( get_template_directory_uri() ).'/css/bootstrap.min.css' );
-      wp_enqueue_style( 'videoproject', esc_url( get_template_directory_uri() ).'/style.css', array('bootstrap.css') );
-
       wp_enqueue_script( 'jquery.tubular', esc_url( get_template_directory_uri() ).'/js/jquery.tubular.1.0.js', array('jquery') );
     }
 /*
