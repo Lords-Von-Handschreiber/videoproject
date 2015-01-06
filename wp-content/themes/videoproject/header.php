328a1a3ac61d6f832eeaef45a32c2e0ea9f25de4
Thomas Bandixen <thomas@bandixen.ch>
2015-01-06 18:02:56 +0100

    updated

--- wp-content/themes/videoproject/header.php
+++ wp-content/themes/videoproject/header.php
@@ -10,6 +10,7 @@
     <script src="http://code.jquery.com/jquery-latest.js"></script>
 
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+    <?php wp_head(); ?>
 </head>
 <body>
 <div id="wrapper">
