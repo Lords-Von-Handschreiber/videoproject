ca7d53d7267758e02040d9067067d393b27683bd
Thomas Bandixen <thomas@bandixen.ch>
2015-01-06 19:22:31 +0100

    updated

--- wp-content/plugins/backgroundvideo/backgroundvideo.php
+++ wp-content/plugins/backgroundvideo/backgroundvideo.php
@@ -26,6 +26,7 @@ class BackgroundVideo{
 
       wp_enqueue_script( 'bootstrap', esc_url( get_template_directory_uri() ).'/js/bootstrap.min.js', array('jquery') );
       wp_enqueue_style( 'bootstrap.css', esc_url( get_template_directory_uri() ).'/css/bootstrap.min.css' );
+      wp_enqueue_style( 'videoproject', esc_url( get_template_directory_uri() ).'/style.css', array('bootstrap.css') );
 
       wp_enqueue_script( 'jquery.tubular', esc_url( get_template_directory_uri() ).'/js/jquery.tubular.1.0.js', array('jquery') );
     }
--- wp-content/themes/videoproject/header.php
+++ wp-content/themes/videoproject/header.php
@@ -5,8 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>VideoProject Theme</title>
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-
     <?php wp_head(); ?>
 </head>
 <body>
